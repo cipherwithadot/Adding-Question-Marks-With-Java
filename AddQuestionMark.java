bcb7525b4c46ab576a62a7b8ffadd9b7fc0f5afb
Cipher <88819188+Cipher-GF@users.noreply.github.com>
2022-06-07 12:49:38 +0530

    Create AddQuestionMark.java

---
+++ AddQuestionMark.java
@@ -0,0 +1,13 @@
+public class AddQuestionMark
+{
+    public static void AddQuestionMark(String q)
+    {    
+      System.out.println(q + "?");
+    }
+
+    public static void main (String args[]){
+    
+    AddQuestionMark("cipher");
+    
+    }
+}
